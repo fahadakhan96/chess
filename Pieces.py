399938461043ad103e36802f843df71a87d52b37
Fahad Ahmed Khan <fahadakhan96@hotmail.com>
2016-11-04 10:36:15 +0500

    Drawing complete board now

--- Pieces.py
+++ Pieces.py
@@ -4,6 +4,7 @@ spriteimage = pyglet.resource.image('resources/spritesheet.png')
 spritesheet = pyglet.image.ImageGrid(spriteimage, 2, 6)
 BLACK_KING, BLACK_QUEEN, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK, BLACK_PAWN, WHITE_KING, WHITE_QUEEN, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK, WHITE_PAWN = range(12)
 
+
 class Piece(object):
     white = True
     piecesprite = None
@@ -40,6 +41,7 @@ class Rook(Piece):
             self.pieceimage = spritesheet[BLACK_ROOK]
         self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
 
+
 class Knight(Piece):
     def __init__(self, x, y, type=True):
         super(Knight, self).__init__(x, y, type)
@@ -47,4 +49,31 @@ class Knight(Piece):
             self.pieceimage = spritesheet[WHITE_KNIGHT]
         else:
             self.pieceimage = spritesheet[BLACK_KNIGHT]
+        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
+
+class Bishop(Piece):
+    def __init__(self, x, y, type=True):
+        super(Bishop, self).__init__(x, y, type)
+        if self.white:
+            self.pieceimage = spritesheet[WHITE_BISHOP]
+        else:
+            self.pieceimage = spritesheet[BLACK_BISHOP]
+        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
+
+class Queen(Piece):
+    def __init__(self, x, y, type=True):
+        super(Queen, self).__init__(x, y, type)
+        if self.white:
+            self.pieceimage = spritesheet[WHITE_QUEEN]
+        else:
+            self.pieceimage = spritesheet[BLACK_QUEEN]
+        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
+
+class King(Piece):
+    def __init__(self, x, y, type=True):
+        super(King, self).__init__(x, y, type)
+        if self.white:
+            self.pieceimage = spritesheet[WHITE_KING]
+        else:
+            self.pieceimage = spritesheet[BLACK_KING]
         self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
--- guiChess.py
+++ guiChess.py
@@ -20,10 +20,18 @@ class Chess(pyglet.window.Window):
         self.whitepieces.append(p.Rook(7, 0))
         self.whitepieces.append(p.Knight(1, 0))
         self.whitepieces.append(p.Knight(6, 0))
+        self.whitepieces.append(p.Bishop(2, 0))
+        self.whitepieces.append(p.Bishop(5, 0))
+        self.whitepieces.append(p.Queen(3, 0))
+        self.whitepieces.append(p.King(4, 0))
         self.blackpieces.append(p.Rook(0, 7, False))
         self.blackpieces.append(p.Rook(7, 7, False))
         self.blackpieces.append(p.Knight(1, 7, False))
         self.blackpieces.append(p.Knight(6, 7, False))
+        self.blackpieces.append(p.Bishop(2, 7, False))
+        self.blackpieces.append(p.Bishop(5, 7, False))
+        self.blackpieces.append(p.Queen(3, 7, False))
+        self.blackpieces.append(p.King(4, 7, False))
 
     def on_draw(self):
         self.chessboard.blit(0, 0)
