4ac22196ff94a46c1c6d3da7edf42149fb9f337c
Fahad Ahmed Khan <fahadakhan96@hotmail.com>
2016-11-06 00:12:27 +0500

    Added Knight movements

--- Pieces.py
+++ Pieces.py
@@ -116,6 +116,45 @@ class Knight(Piece):
             self.pieceimage = spritesheet[BLACK_KNIGHT]
         self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, x * 75, y * 75)
 
+    def GetValidMoves(self, board):
+        x = self.piecesprite.x // 75
+        y = self.piecesprite.y // 75
+        ListOfMoves = []
+        try:
+            if board[y+2][x+1] is None or self.white != board[y+2][x+1].white:
+                ListOfMoves.append((y+2, x+1))
+        except IndexError:
+            pass
+        try:
+            if x > 0 and (board[y+2][x-1] is None or self.white != board[y+2][x-1].white):
+                ListOfMoves.append((y+2, x-1))
+        except IndexError:
+            pass
+        try:
+            if board[y+1][x+2] is None or self.white != board[y+1][x+2].white:
+                ListOfMoves.append((y+1, x+2))
+        except IndexError:
+            pass
+        try:
+            if x > 1 and (board[y+1][x-2] is None or self.white != board[y+1][x-2].white):
+                ListOfMoves.append((y+1, x-2))
+        except IndexError:
+            pass
+        try:
+            if y > 0 and (board[y-1][x+2] is None or self.white != board[y-1][x+2].white):
+                ListOfMoves.append((y-1, x+2))
+        except IndexError:
+            pass
+        try:
+            if y > 1 and (board[y-2][x+1] is None or self.white != board[y-2][x+1].white):
+                ListOfMoves.append((y-2, x+1))
+        except IndexError:
+            pass
+        if x > 1 and y > 0 and (board[y-1][x-2] is None or self.white != board[y-1][x-2].white):
+            ListOfMoves.append((y-1, x-2))
+        if x > 0 and y > 1 and (board[y-2][x-1] is None or self.white != board[y-2][x-1].white):
+            ListOfMoves.append((y-2, x-1))
+        return ListOfMoves
 
 class Bishop(Piece):
     def __init__(self, x, y, type=True):
