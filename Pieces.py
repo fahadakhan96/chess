747f226633faec00da8717538bf64162defbb724
Fahad Ahmed Khan <fahadakhan96@hotmail.com>
2016-11-05 19:14:49 +0500

    Pawn's are moving (only vertically, no obstruction detection yet)

--- Pieces.py
+++ Pieces.py
@@ -2,21 +2,28 @@ import pyglet
 
 spriteimage = pyglet.resource.image('resources/spritesheet.png')
 spritesheet = pyglet.image.ImageGrid(spriteimage, 2, 6)
-BLACK_KING, BLACK_QUEEN, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK, BLACK_PAWN, WHITE_KING, WHITE_QUEEN, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK, WHITE_PAWN = range(12)
+BLACK_KING, BLACK_QUEEN, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK, BLACK_PAWN, WHITE_KING, WHITE_QUEEN, WHITE_BISHOP, \
+    WHITE_KNIGHT, WHITE_ROOK, WHITE_PAWN = range(12)
 
 
 class Piece(object):
     white = True
     piecesprite = None
     captured = False
-    x = 0
-    y = 0
+    # x = 0
+    # y = 0
 
-    def __init__(self, x, y, type):
-        self.x = x
-        self.y = y
+    def __init__(self, type):
+        # self.x = x
+        # self.y = y
         self.white = type
         self.captured = False
+
+    def ChangeLocation(self, x, y):
+        # self.x = x
+        # self.y = y
+        self.piecesprite.x = x * 75
+        self.piecesprite.y = y * 75
 
     def Draw(self):
         self.piecesprite.draw()
@@ -24,56 +31,73 @@ class Piece(object):
 
 class Pawn(Piece):
     def __init__(self, x, y, type=True):
-        super(Pawn, self).__init__(x, y, type)
+        super(Pawn, self).__init__(type)
         if self.white:
             self.pieceimage = spritesheet[WHITE_PAWN]
         else:
             self.pieceimage = spritesheet[BLACK_PAWN]
-        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
+        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, x * 75, y * 75)
+
+    def GetValidMoves(self):
+        ListOfMoves = []
+        if self.white:
+            ListOfMoves.append((self.piecesprite.y//75 + 1, self.piecesprite.x//75))
+            if (self.piecesprite.y//75 == 1):
+                ListOfMoves.append((self.piecesprite.y//75 + 2, self.piecesprite.x//75))
+            return ListOfMoves
+        else:
+            ListOfMoves.append((self.piecesprite.y // 75 - 1, self.piecesprite.x // 75))
+            if (self.piecesprite.y // 75 == 6):
+                ListOfMoves.append((self.piecesprite.y // 75 - 2, self.piecesprite.x // 75))
+            print ListOfMoves
+            return ListOfMoves
 
 
 class Rook(Piece):
     def __init__(self, x, y, type=True):
-        super(Rook, self).__init__(x, y, type)
+        super(Rook, self).__init__(type)
         if self.white:
             self.pieceimage = spritesheet[WHITE_ROOK]
         else:
             self.pieceimage = spritesheet[BLACK_ROOK]
-        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
+        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, x * 75, y * 75)
 
 
 class Knight(Piece):
     def __init__(self, x, y, type=True):
-        super(Knight, self).__init__(x, y, type)
+        super(Knight, self).__init__(type)
         if self.white:
             self.pieceimage = spritesheet[WHITE_KNIGHT]
         else:
             self.pieceimage = spritesheet[BLACK_KNIGHT]
-        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
+        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, x * 75, y * 75)
+
 
 class Bishop(Piece):
     def __init__(self, x, y, type=True):
-        super(Bishop, self).__init__(x, y, type)
+        super(Bishop, self).__init__(type)
         if self.white:
             self.pieceimage = spritesheet[WHITE_BISHOP]
         else:
             self.pieceimage = spritesheet[BLACK_BISHOP]
-        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
+        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, x * 75, y * 75)
+
 
 class Queen(Piece):
     def __init__(self, x, y, type=True):
-        super(Queen, self).__init__(x, y, type)
+        super(Queen, self).__init__(type)
         if self.white:
             self.pieceimage = spritesheet[WHITE_QUEEN]
         else:
             self.pieceimage = spritesheet[BLACK_QUEEN]
-        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
+        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, x * 75, y * 75)
+
 
 class King(Piece):
     def __init__(self, x, y, type=True):
-        super(King, self).__init__(x, y, type)
+        super(King, self).__init__(type)
         if self.white:
             self.pieceimage = spritesheet[WHITE_KING]
         else:
             self.pieceimage = spritesheet[BLACK_KING]
-        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, self.x * 75, self.y * 75)
+        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, x * 75, y * 75)
--- guiChess.py
+++ guiChess.py
@@ -1,9 +1,13 @@
 import pyglet
+from pyglet.window import mouse
 import Pieces as p
 
 
 class Chess(pyglet.window.Window):
     chessboard = pyglet.resource.image('resources/chessboard.png')
+    validImg = pyglet.resource.image('resources/validmove.png')
+    # hoverImg = pyglet.resource.image('resources/hoversquare.png')
+    currentPos = (-1, -1)
 
     def __init__(self):
         super(Chess, self).__init__(600, 600,
@@ -11,30 +15,51 @@ class Chess(pyglet.window.Window):
                                     caption='Sudoku',
                                     config=pyglet.gl.Config(double_buffer=True),
                                     vsync=False)
-        self.whitepieces = []
-        self.blackpieces = []
+        self.board = [[p.Rook(0, 0), p.Knight(1, 0), p.Bishop(2, 0), p.Queen(3, 0), p.King(4, 0), p.Bishop(5, 0),
+                       p.Knight(6, 0), p.Rook(7,0)],
+                      [p.Pawn(i, 1) for i in range(8)],
+                      [None for i in range(8)],
+                      [None for i in range(8)],
+                      [None for i in range(8)],
+                      [None for i in range(8)],
+                      [p.Pawn(i, 6, False) for i in range(8)],
+                      [p.Rook(0, 7, False), p.Knight(1, 7, False), p.Bishop(2, 7, False), p.Queen(3, 7, False),
+                       p.King(4, 7, False), p.Bishop(5, 7, False), p.Knight(6, 7, False), p.Rook(7, 7, False)]]
+        self.validsprites = []
         for i in range(8):
-            self.whitepieces.append(p.Pawn(i, 1))
-            self.blackpieces.append(p.Pawn(i, 6, False))
-        self.whitepieces.append(p.Rook(0, 0))
-        self.whitepieces.append(p.Rook(7, 0))
-        self.whitepieces.append(p.Knight(1, 0))
-        self.whitepieces.append(p.Knight(6, 0))
-        self.whitepieces.append(p.Bishop(2, 0))
-        self.whitepieces.append(p.Bishop(5, 0))
-        self.whitepieces.append(p.Queen(3, 0))
-        self.whitepieces.append(p.King(4, 0))
-        self.blackpieces.append(p.Rook(0, 7, False))
-        self.blackpieces.append(p.Rook(7, 7, False))
-        self.blackpieces.append(p.Knight(1, 7, False))
-        self.blackpieces.append(p.Knight(6, 7, False))
-        self.blackpieces.append(p.Bishop(2, 7, False))
-        self.blackpieces.append(p.Bishop(5, 7, False))
-        self.blackpieces.append(p.Queen(3, 7, False))
-        self.blackpieces.append(p.King(4, 7, False))
+            rowsprites = []
+            for j in range(8):
+                sprite = pyglet.sprite.Sprite(self.validImg, 75 * j, 75 * i)
+                sprite.visible = False
+                rowsprites.append(sprite)
+            self.validsprites.append(rowsprites)
 
     def on_draw(self):
+        self.clear()
         self.chessboard.blit(0, 0)
-        for i in range(len(self.whitepieces)):
-            self.whitepieces[i].Draw()
-            self.blackpieces[i].Draw()
+        for i in range(8):
+            for j in range(8):
+                self.validsprites[i][j].draw()
+                if self.board[i][j] is not None: self.board[i][j].Draw()
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        boardX = x//75
+        boardY = y//75
+        if self.currentPos[0] < 0 and self.currentPos[1] < 0:
+            if self.board[boardY][boardX] is not None:
+                self.currentPos = (boardY, boardX)
+                ValidMoves = self.board[boardY][boardX].GetValidMoves()
+                for move in ValidMoves:
+                    self.validsprites[move[0]][move[1]].visible = True
+        else:
+            if self.validsprites[boardY][boardX].visible:
+                self.board[self.currentPos[0]][self.currentPos[1]].ChangeLocation(boardX, boardY)
+                self.board[self.currentPos[0]][self.currentPos[1]], self.board[boardY][boardX] = self.board[boardY][boardX], self.board[self.currentPos[0]][self.currentPos[1]]
+                self.currentPos = (-1, -1)
+                for row in self.validsprites:
+                    for sprite in row:
+                        sprite.visible = False
+
+
+    def update(self, dt):
+        self.on_draw()
